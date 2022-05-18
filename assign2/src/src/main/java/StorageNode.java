2946f155f836794ad0018d6577c9d7746b347168
unknown <up201904996@fe.up.pt>
2022-05-18 10:41:27 +0100

    just added steps for join

--- assign2/src/src/main/java/StorageNode.java
+++ assign2/src/src/main/java/StorageNode.java
@@ -43,6 +43,27 @@ public class StorageNode implements Functions, Remote {
 
     @Override
     public String join() throws RemoteException, InterruptedException, ExecutionException {
+
+        /*
+        1. Every time a node joins or leaves the cluster, the node must send via IP multicast a JOIN/LEAVE message, respectively.
+        2. Messages:
+            - membership counter (initially 0) - increases everytime the node join or leaves the cluster
+                if even the node is joining the cluster, if odd the node is leaving
+                The membership counter must survive node crashes and therefore should be stored in non-volatile memory.
+        3. Add events (join/leave) to the membership log, this must contain:
+            - node id
+            - membership counter
+        4. Whenever a node JOINS initialize the cluster membership aka some nodes will send a Membership message, which includes:
+            - list of current cluster members
+            - the 32 most recent membership events (put, join, leave, etc) in its log.
+            Note: Use TCP to pass membership information to nodes joining the cluster
+        5. Since a new member starts accepting connections on a PORT before sending JOIN messages, it sends the port number on these messages.
+        6. X time after receiving the join message, a cluster member send the membership information.
+            Note: The new member stops accepting connections after receiving 3 MEMBERSHIP messages.
+        7.
+
+
+         */
         ScheduledFuture scheduledFuture = scheduledExecutorService.schedule(new UDPMulticastServer("JOIN"),0, TimeUnit.SECONDS);
         return scheduledFuture.get().toString();
     }
