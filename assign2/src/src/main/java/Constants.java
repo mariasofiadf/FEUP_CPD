65fe37e42d9ebcc79e48d151d1576dddab73256d
Maria Sofia Figueiredo <up201904675@fe.up.pt>
2022-05-31 11:30:32 +0100

    removed unused constants

--- assign2/src/src/main/java/Constants.java
+++ assign2/src/src/main/java/Constants.java
@@ -4,8 +4,6 @@ public final class Constants {
     public static final String LOG = "log";
     public static final String ADDRESS = "address";
     public static final String PORT = "port";
-    public static final String SS_CHANNEL = "SS_CHANNEL";
-    public static final String SOCKET_CHANNEL = "SOCKET_CHANNEL";
     public static final Integer MAX_JOIN_TRIES = 3;
     public static final Integer MIN_RECEIVED_MEMBERSHIP = 3;
     public static final String GET = "get";
@@ -27,24 +25,15 @@ public final class Constants {
     public static final Integer MAX_THREADS = 1000;
     public static final Integer MAX_LOG = 32;
 
-    public static final String CHANNEL_MCAST = "CHANNEL_MCAST";
 
-    public static final String CHANNEL_MEMBERSHIP = "CHANNEL_MEMBERSHIP";
-
-    public static final String CHANNEL_APOINT = "CHANNEL_APOINT";
-
-    public static final String CHANNEL_UCAST = "CHANNEL_UCAST";
-    public static final String CHANNEL_FILES_WRITE = "CHANNEL_FILES_WRITE";
-    public static final String CHANNEL_FILES_READ ="CHANNEL_FILES_READ" ;
-
-    public static final Integer MEMBERSHIP_INTERVAL = 2;
+    public static final Integer LOG_INTERVAL = 2;
     public static final String PUT = "put";
     public static final String KEY = "key";
 
-    public static String MEMBERS_FILENAME = "members";
-    public static String LOG_FILENAME = "log";
-    public static String STORE_FOLDER = "store";
+    public static final String MEMBERS_FILENAME = "members";
+    public static final String LOG_FILENAME = "log";
+    public static final String STORE_FOLDER = "store";
 
-    public static Boolean DEBUG = true;
+    public static final Boolean DEBUG = true;
 
 }
