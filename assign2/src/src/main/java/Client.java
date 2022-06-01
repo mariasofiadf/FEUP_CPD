dad67115327c6598238647a9a56615f0417e05e5
Luísa Araújo <lmpa.pt@gmail.com>
2022-06-01 22:23:30 +0100

    client java

--- assign2/src/src/main/java/Client.java
+++ assign2/src/src/main/java/Client.java
@@ -1,11 +1,23 @@
 import java.rmi.registry.Registry;
-import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.rmi.registry.LocateRegistry;
-
+import java.io.File;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Client {
-    public static void main(String[] args) {
+
+    StorageNode node;
+    String node_ap;
+    String operation;
+    String opnd;
+
+    public Client(StorageNode node){
+        this.node = node;
+    }
+
+    public void main(String[] args) {
 
         try {
             Registry registry = LocateRegistry.getRegistry();
@@ -18,8 +30,8 @@ public class Client {
         }
 
         try{
-            var node_ap = args[0];
-            String operation = args[1];
+            node_ap = args[0];
+            operation = args[1];
 
             //node_ap = <IP address>:<port number>
             //if ip == remi -> ip address and the name of the remote object providing the service
@@ -27,15 +39,24 @@ public class Client {
             //opnd -> put: pathname, get or delete: hexa string the key
 
             //rmi service is used
-            InetAddress ip = InetAddress.getLocalHost();
-            String name = ip.getHostName();
-
+            //InetAddress ip = InetAddress.getLocalHost();
+            //String name = ip.getHostName();
+            
             String delimiter = ":";
             int end = node_ap.indexOf(delimiter);
             //find delimiter if end != -1
             if(end != -1){
-                //InetAddress ip = node_ap.substring(0, end);
+                String ipAux = node_ap.substring(0, end);
+                InetAddress ip = InetAddress.getByName(ipAux);
+                
+                String nameAux = node_ap.substring(end + 1, node_ap.length());
+                
+                if(nameAux != ip.getHostName()){
+                    System.out.println("Host name for the given IP doesn't correspond to the IP's host name.");
+                }
             }
+
+            //criar new storage node
 
             switch(operation){
                 case "put":
@@ -45,8 +66,10 @@ public class Client {
                 case "delete":
                     break;
                 case "join":
+                    node.join();
                     break;
                 case "leave":
+                    node.leave();
                     break;
                 default:
                     System.out.println("Invalid operation");
@@ -55,12 +78,36 @@ public class Client {
 
             if(operation != "join" && operation != "leave"){
                 try{
+
                     var opnd = args[2];
-                    if(operation == "put"){
-                        String pathname = opnd;
+                    String key = new Hash().hash(opnd);
+                    String str;
+
+                    switch(operation){
+
+                        case "put":
+                            //opnd here is the pathname of the file that contains the bytes to store
+                            str = Files.readString(Paths.get(opnd));
+                            node.put(key, str.getBytes());
+                            System.out.println("Put " + opnd + "!");
+                            break;
+                        
+                        case "get":
+                            //opnd here is a key
+                            str = node.get(key);
+                            System.out.println("File " + opnd + ": \n" + str);
+                            break;
+
+                        case "delete":
+                            str = node.delete(key);
+                            System.out.println("Deleted " + opnd + "!");
+                            break;
                     }
-                    else if(operation == "get" || operation == "delete"){
-                        var key = opnd;
+                    if(operation == "put"){
+                        
+                    }
+                    else if(operation == "get"){
+                        
                     }
                     else{
                         System.out.println("Not a valid operand for the above operation.");
@@ -74,5 +121,6 @@ public class Client {
         catch(Exception e){
             System.out.println("Incorrect arguments.");
         }
+
     }
 }
---
+++ assign2/src/src/main/java/Reader.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Reader {
+
+    String path;
+
+
+    public String readContent(){
+
+        File f = new File(opnd);
+        Scanner reader = new Scanner(f);
+        while()
+        return "";
+    }
+    
+}
